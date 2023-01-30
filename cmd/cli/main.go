100625a08747bee73edbbb85fb60f493d9c92df5
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2023-01-30 20:59:59 +0100

    major implementation refactoring

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/go-git/go-git/v5"
+	"github.com/jaedle/test-and-commit-or-revert/internal"
 	"os"
-	"os/exec"
 )
 
 type config struct {
@@ -13,106 +10,15 @@ type config struct {
 }
 
 func main() {
-	println("tcr")
-
-	_, err := os.Stat(".git")
+	result, err := internal.New(internal.Config{
+		Workdir: ".",
+	}).Run()
 	if err != nil {
 		os.Exit(1)
-	}
-
-	println("reading config")
-
-	_, err = os.Stat("tcr.json")
-	if err != nil {
+	} else if *result == internal.Failure {
 		os.Exit(1)
-	}
-
-	file, err := os.Open("tcr.json")
-	if err != nil {
-		os.Exit(1)
-	}
-	defer func() { _ = file.Close() }()
-
-	var c config
-	if err := json.NewDecoder(file).Decode(&c); err != nil {
-		os.Exit(1)
-	}
-
-	println("opening repository")
-
-	repo, err := git.PlainOpen(".")
-	if err != nil {
-		os.Exit(1)
-	}
-
-	if passed, err := test(c); err != nil {
-		os.Exit(1)
-	} else if passed {
-		commit(repo)
 	} else {
-		revert(repo)
-		os.Exit(1)
+		os.Exit(0)
 	}
-
-}
-
-func test(c config) (bool, error) {
-	println("running tests")
-
-	cmd := exec.Command(c.Test)
-	err := cmd.Run()
-
-	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
-		println("exit error", err.Error())
-		return false, nil
-	} else if err != nil {
-		println("general error", err.Error())
-		return false, err
-	} else {
-		return true, nil
-	}
-}
-
-func commit(repo *git.Repository) {
-	worktree, err := repo.Worktree()
-	if err != nil {
-		os.Exit(1)
-	}
-
-	if err := worktree.AddGlob("*"); err != nil {
-		os.Exit(1)
-	}
-
-	_, err = worktree.Commit("[WIP] refactoring", &git.CommitOptions{})
-	if err != nil {
-		os.Exit(1)
-	}
-}
-
-func revert(repo *git.Repository) {
-	worktree, err := repo.Worktree()
-	if err != nil {
-		os.Exit(1)
-	}
-	fmt.Println("cleaned")
-
-	//err = worktree.Clean(&git.CleanOptions{})
-	//if err != nil {
-	//	os.Exit(1)
-	//}
-
-	err = worktree.Reset(&git.ResetOptions{
-		Mode: git.HardReset,
-	})
-	if err != nil {
-		os.Exit(1)
-	}
-
-	status, err := worktree.Status()
-	if err != nil {
-		os.Exit(1)
-	}
-
-	println(status.String())
 
 }
---
+++ internal/tcr.go
@@ -0,0 +1,131 @@
+package internal
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"os"
+	"os/exec"
+)
+
+type Result int
+
+const (
+	Success Result = iota
+	Failure Result = iota
+)
+
+type config struct {
+	Test string `json:"test"`
+}
+
+func New(c Config) *Tcr {
+	return &Tcr{}
+}
+
+type Tcr struct {
+	config config
+	repo   *git.Repository
+}
+
+func (t *Tcr) Run() (*Result, error) {
+	if err := t.readConfig(); err != nil {
+		return nil, err
+	}
+
+	if err := t.openRepository(); err != nil {
+		return nil, err
+	}
+
+	if passed, err := t.test(); err != nil {
+		return nil, err
+	} else if passed {
+		return t.commit()
+	} else {
+		if err := t.revert(); err != nil {
+			return nil, err
+		}
+
+		var result = Failure
+		return &result, nil
+	}
+
+}
+
+func (t *Tcr) readConfig() error {
+	file, err := os.Open("tcr.json")
+	if err != nil {
+		return err
+	}
+	defer func() { _ = file.Close() }()
+
+	var c config
+	if err := json.NewDecoder(file).Decode(&c); err != nil {
+		return err
+	}
+
+	t.config = c
+	return nil
+}
+
+func (t *Tcr) commit() (*Result, error) {
+	wt, err := t.repo.Worktree()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := wt.AddGlob("*"); err != nil {
+		return nil, err
+	}
+
+	if _, err := wt.Commit("[WIP] refactoring", &git.CommitOptions{}); err != nil {
+		return nil, err
+	}
+
+	var result = Success
+	return &result, nil
+}
+
+func (t *Tcr) test() (bool, error) {
+	println("running tests")
+
+	cmd := exec.Command(t.config.Test)
+	err := cmd.Run()
+
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		println("exit error", err.Error())
+		return false, nil
+	} else if err != nil {
+		println("general error", err.Error())
+		return false, err
+	} else {
+		return true, nil
+	}
+	return false, nil
+}
+
+func (t *Tcr) openRepository() error {
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		return err
+	}
+
+	t.repo = repo
+	return nil
+}
+
+func (t *Tcr) revert() error {
+	worktree, err := t.repo.Worktree()
+	if err != nil {
+		return nil
+	}
+	fmt.Println("cleaned")
+
+	return worktree.Reset(&git.ResetOptions{
+		Mode: git.HardReset,
+	})
+}
+
+type Config struct {
+	Workdir string
+}
